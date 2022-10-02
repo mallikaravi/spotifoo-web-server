d38ee6f2e46c8db8b1287e1e9a3f37b600b8fb9c
mallikaravi <mailtomallikaravi@gmail.com>
2022-10-02 21:40:45 +0200

    updated version

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ var indexRouter = require('./routes/music');
 
 var app = express();
 
+//https://stackoverflow.com/a/6661431/19625369
+app.use(function(req, res, next) {
+    //https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
